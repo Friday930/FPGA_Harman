5ee235449a2ec0aa132f92b490a7e8dadaf9530a
Friday930 <tukankr@khu.ac.kr>
2025-03-04 15:18:24 +0900

    clk divider 추가

--- 20250304_adder_fnd/20250304_adder_fnd.srcs/sources_1/imports/new/fnd_controller.v
+++ 20250304_adder_fnd/20250304_adder_fnd.srcs/sources_1/imports/new/fnd_controller.v
@@ -9,9 +9,16 @@ module fnd_controller(
 
     wire [3:0] w_digit_1, w_digit_10, w_digit_100, w_digit_1000, w_bcd;
     wire [1:0] w_seg_sel;
+    wire w_clk_100Hz;
+
+    clk_divider U_Clk_Divider(
+        .clk(clk),
+        .reset(reset),
+        .o_clk(w_clk_100Hz)
+    );
 
     counter_4 U_counter_4(
-        .clk(clk),
+        .clk(w_clk_100Hz),
         .reset(reset),
         .o_sel(w_seg_sel)
     );
@@ -104,6 +111,33 @@ module mux_4x1 (
         endcase
     end
 
+endmodule
+
+module clk_divider(
+    input clk, reset,
+    output o_clk
+);
+    parameter FCOUNT = 500_000;
+    reg [$clog2(FCOUNT)-1:0] r_counter; // $clog2 : 숫자를 나타내는데 필요한 비트 수 계산
+    reg r_clk;
+
+    assign o_clk = r_clk;
+
+    always @(posedge clk, posedge reset) begin
+        if(reset) begin
+            r_counter <= 0; // 리셋 상태
+            r_clk <= 1'b0;
+        end else begin
+            if (r_counter == FCOUNT - 1) begin // clk divide 계산, 100MHz -> 100Hz
+                r_counter <= 0;
+                r_clk <= 1'b1; // r_clk을 99999999번째 posedge에 1로 바꿈 r_clk : 0->1
+            end else begin
+                r_counter <= r_counter + 1;
+                r_clk <= 1'b0; // r_clk : 1->0 or 0으로 유지
+            end
+        end
+    end
+    
 endmodule
 
 module counter_4 (
