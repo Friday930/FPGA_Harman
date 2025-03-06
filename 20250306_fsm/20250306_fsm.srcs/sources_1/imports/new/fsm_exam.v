87bdfad258d395dfad94a38ed3d4a99b006a8183
Friday930 <tukankr@khu.ac.kr>
2025-03-06 11:32:52 +0900

    mealy

--- 20250306_fsm/20250306_fsm.srcs/sources_1/imports/new/fsm_exam.v
+++ 20250306_fsm/20250306_fsm.srcs/sources_1/imports/new/fsm_exam.v
@@ -16,7 +16,7 @@ module fsm_exam(
 
     always @(posedge clk, posedge reset) begin
         if(reset) begin
-            state <= 2'b00;
+            state <= 3'b000;
         end else begin
             state <= next;
         end
@@ -25,47 +25,79 @@ module fsm_exam(
     always @(*) begin
         next = state;
         case (state)
-            IDLE: if(sw == 3'b010) begin
-                next = ST2;
-            end else if(sw == 3'b001) begin
-                next = ST1;
-            end else begin
-                next = state;
+            IDLE: begin 
+                if(sw == 3'b010) begin
+                    next = ST2;
+                end else if(sw == 3'b001) begin
+                    next = ST1;
+                end else begin
+                    next = state;
+                end
             end
 
-            ST1: if(sw == 3'b010) begin
-                next = ST2;
-            end else next <= state;
-
-            ST2: if(sw == 3'b100) begin
-                next = ST3;
-            end else next = state;
-
-            ST3: if(sw == 3'b000) begin
-                next = IDLE;
-            end else if(sw == 3'b001) begin
-                next = ST1;
-            end else if(sw == 3'b111) begin
-                next = ST4;
-            end else begin
-                next = state;
+            ST1: begin
+                if(sw == 3'b010) begin
+                    next = ST2;
+                end else next <= state;
             end
 
-            ST4: if(sw == 3'b100) begin
-                next = ST3;
-            end else next = state;
+            ST2: begin
+                if(sw == 3'b100) begin
+                    next = ST3;
+                end else next = state;
+            end
 
+            ST3: begin
+                if(sw == 3'b000) begin
+                    next = IDLE;
+                end else if(sw == 3'b001) begin
+                    next = ST1;
+                end else if(sw == 3'b111) begin
+                    next = ST4;
+                end else begin
+                    next = state;
+                end
+            end
+
+            ST4: begin
+                if(sw == 3'b100) begin
+                    next = ST3;
+                end else next = state;
+            end
             default: next = state;
         endcase
     end
 
     always @(*) begin
             case (state)
-                IDLE: r_led = 3'b000;
-                ST1: r_led = 3'b001;
-                ST2: r_led = 3'b010;
-                ST3: r_led = 3'b100;
-                ST4: r_led = 3'b111;
+                IDLE: begin
+                    // r_led = 3'b000; // moore
+                    if(sw == 3'b001) r_led = 3'b001;// mealy
+                    else if(sw == 3'b010) r_led = 3'b010;
+                    else r_led = 3'b000;
+                end 
+
+                ST1: begin
+                    if(sw == 3'b001) r_led = 3'b001;
+                    else r_led = 3'b001;
+                end
+
+                ST2: begin
+                    if(sw == 3'b010) r_led = 3'b010;
+                    else r_led = 3'b100;
+                end
+
+                ST3: begin
+                    if(sw == 3'b000) r_led = 3'b000;
+                    else if(sw == 3'b001) r_led = 3'b001;
+                    else if(sw == 3'b111) r_led = 3'b111;
+                    else r_led = 3'b101;
+                end 
+                ST4: begin
+                    if(sw == 3'b100) r_led = 3'b100;
+                    else r_led = 3'b111;
+                end
+
                 default: r_led = 3'b000;
             endcase
     end
