97cedd91494b93534be1f51cfbc14acef86bbef2
Friday930 <tukankr@khu.ac.kr>
2025-03-13 12:56:38 +0900

    1차 완성

--- 20250312_stopwatch/20250312_stopwatch.srcs/sources_1/imports/sources_1/new/top_stopwatch.v
+++ 20250312_stopwatch/20250312_stopwatch.srcs/sources_1/imports/sources_1/new/top_stopwatch.v
@@ -11,7 +11,8 @@ module top_stopwatch(
     wire                w_clk_100Hz;
     wire                w_msec_tick, w_sec_tick, w_minute_tick;
     wire                w_run, w_clear, run, clear; // 반드시 선언
-    wire                w_btn_hour, w_btn_min, w_btn_sec;
+    wire                [4:0] w_btn_hour;
+    wire                [5:0] w_btn_min, w_btn_sec;
     wire                cs, cs_inv;
 
     wire                [6:0] msec;
@@ -19,21 +20,32 @@ module top_stopwatch(
     wire                [5:0] minute;
     wire                [4:0] hour;
 
+    wire                [5:0] ccu_sec;
+    wire                [5:0] ccu_minute;
+    wire                [4:0] ccu_hour;
+    
+    wire                [6:0] sdp_msec;
+    wire                [5:0] sdp_sec;
+    wire                [5:0] sdp_minute;
+    wire                [4:0] sdp_hour;
+
     wire                [6:0] c_msec;
     wire                [5:0] c_sec; 
     wire                [5:0] c_minute;
     wire                [4:0] c_hour;
 
+    assign              cs = sw[1];
+    assign              cs_inv = ~sw[1];
 
     stopwatch_dp U_StopWatch_DP(
         .clk            (clk),
         .reset          (reset),
         .run            (run),
         .clear          (clear),
-        .msec           (msec),
-        .sec            (sec),
-        .minute         (minute),
-        .hour           (hour)
+        .msec           (sdp_msec),
+        .sec            (sdp_sec),
+        .minute         (sdp_minute),
+        .hour           (sdp_hour)
     );
 
     
@@ -79,15 +91,30 @@ module top_stopwatch(
     .c_minute           (w_btn_min),
     .c_hour             (w_btn_hour),
     .cs                 (cs),
-    .o_c_sec            (),     // 비트 폭 수정 (6비트)
-    .o_c_minute         (),  // 비트 폭 수정 (6비트)
-    .o_c_hour           ()     // 비트 폭 수정 (5비트)
+    .o_c_sec            (ccu_sec),     // 비트 폭 수정 (6비트)
+    .o_c_minute         (ccu_minute),  // 비트 폭 수정 (6비트)
+    .o_c_hour           (ccu_hour)     // 비트 폭 수정 (5비트)
     );
 
-    led_indicator U_LED(
-        .sw             (sw),
-        .led            (led)
-    );
+    // led_indicator U_LED(
+    //     .sw             (sw),
+    //     .led            (led)
+    // );
+
+    MUX_7 U_MUX_7(
+    .st_hour            (sdp_hour),
+    .st_minute          (sdp_minute),
+    .st_sec             (sdp_sec),
+    .st_msec            (sdp_msec),
+    .ck_hour            (ccu_hour),
+    .ck_minute          (ccu_minute),
+    .ck_sec             (ccu_sec),
+    .cs                 (cs),
+    .mux_hour           (hour),
+    .mux_minute         (minute),
+    .mux_sec            (sec),
+    .mux_msec           (msec)
+    );    
 
     fnd_controller U_Fnd_Ctrl(
         .clk            (clk), 
@@ -103,19 +130,56 @@ module top_stopwatch(
 endmodule
 
 
-module led_indicator (
-    input               [1:0] sw,
-    output              reg [3:0] led
-);
+// module led_indicator (
+//     input               [1:0] sw,
+//     output              reg [3:0] led
+// );
     
-    always @(*) begin
-        case (sw)
-            2'b00: led = 4'b0001;
-            2'b01: led = 4'b0010;
-            2'b10: led = 4'b0100;
-            2'b11: led = 4'b1000;
-            default: led = 4'bx;
-        endcase
-    end
+//     always @(*) begin
+//         case (sw)
+//             2'b00: led = 4'b0001;
+//             2'b01: led = 4'b0010;
+//             2'b10: led = 4'b0100;
+//             2'b11: led = 4'b1000;
+//             default: led = 4'bx;
+//         endcase
+//     end
 
+// endmodule
+
+module MUX_7 (
+    input               [4:0] st_hour,
+    input               [5:0] st_minute,
+    input               [5:0] st_sec,
+    input               [6:0] st_msec,
+    input               [4:0] ck_hour,
+    input               [5:0] ck_minute,
+    input               [5:0] ck_sec,
+    input               cs,
+    output              reg [4:0] mux_hour,
+    output              reg [5:0] mux_minute,
+    output              reg [5:0] mux_sec,
+    output              reg [6:0] mux_msec
+);
+    always @(*) begin
+        mux_hour = 0;
+        mux_minute = 0;
+        mux_sec = 0;
+        mux_msec = 0;
+
+        case (cs)
+            1'b0: begin
+                mux_hour = st_hour;
+                mux_minute = st_minute;
+                mux_sec = st_sec;
+                mux_msec = st_msec;
+            end
+
+            1'b1: begin
+                mux_hour = ck_hour;
+                mux_minute = ck_minute;
+                mux_sec = ck_sec;
+            end
+        endcase
+    end    
 endmodule
